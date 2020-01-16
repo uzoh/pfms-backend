6ab4119a19511c38e7f88e3940c093d94f510373
Uzoh Okwara <uzohokwara@gmail.com>
2020-01-16 13:34:31 +0100

    add pensioner migration

---
+++ server/models/pensioner.js
@@ -0,0 +1,60 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Pensioner = sequelize.define('Pensioner', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false
+    },
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.CITEXT,
+      allowNull: false,
+      unique: true
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    dob: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    acctNum: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    bank: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nextOfKinName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    nextOfKinPhone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {});
+  Pensioner.associate = function (models) {
+    // associations can be defined here
+  };
+  return Pensioner;
+};
