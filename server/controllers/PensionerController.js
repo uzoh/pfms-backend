372d5e0f9198b214ff02fbb79aa3c30e03f3bb5f
Uzoh Okwara <uzohokwara@gmail.com>
2020-02-17 00:45:19 +0100

    403 Global Error

--- server/controllers/PensionerController.js
+++ server/controllers/PensionerController.js
@@ -1,144 +1,147 @@
-import { validateUniqueResponse, validationResponse } from "@helpers/validationResponse";
-import models from "@models"
-import Response from "@helpers/Response"
-import { validateCreatePensioner, validateUpdatePensioner, validatePayment } from "@validations/pensioner";
+import {
+  validateUniqueResponse,
+  validationResponse
+} from "@helpers/validationResponse";
+import models from "@models";
+import Response from "@helpers/Response";
+import {
+  validateCreatePensioner,
+  validateUpdatePensioner,
+  validatePayment
+} from "@validations/pensioner";
 
-const { Pensioner } = models
+const { Pensioner } = models;
 
 class PensionerController {
-    static async create(req, res, next) {
-        try {
-            const pensionerDetails = await validateCreatePensioner(req.body);
-            const pensioner = await Pensioner.create(pensionerDetails);
+  static async create(req, res, next) {
+    try {
+      const pensionerDetails = await validateCreatePensioner(req.body);
+      const pensioner = await Pensioner.create(pensionerDetails);
 
-            Response.success(res, 200, pensioner);
+      Response.success(res, 200, pensioner);
+    } catch (err) {
+      if (err.isJoi && err.name === "ValidationError") {
+        return res.status(400).json({
+          status: 400,
+          errors: validationResponse(err)
+        });
+      }
 
-        } catch (err) {
-            if (err.isJoi && err.name === 'ValidationError') {
-                return res.status(400).json({
-                    status: 400,
-                    errors: validationResponse(err)
-                });
-            }
-
-            if (err.errors && err.errors[0].type === 'unique violation') {
-                return res.status(400).json({
-                    status: 400,
-                    errors: validateUniqueResponse(err)
-                });
-            }
-            next(err);
-        }
-
+      if (err.errors && err.errors[0].type === "unique violation") {
+        return Response.error(res, 403, "Email already exists");
+      }
+      next(err);
     }
+  }
 
-    static async getall(req, res, next) {
-        try {
-            const pensioner = await Pensioner.findAll();
-            return Response.success(res, 200, pensioner);
-
-        } catch (error) {
-            return Response.error(res, 500, error);
-        }
+  static async getall(req, res, next) {
+    try {
+      const pensioner = await Pensioner.findAll();
+      return Response.success(res, 200, pensioner);
+    } catch (error) {
+      return Response.error(res, 500, error);
     }
+  }
 
-    static async delete(req, res, next) {
-        try {
-            const { pensionerID } = req.params
+  static async delete(req, res, next) {
+    try {
+      const { pensionerID } = req.params;
 
-            const pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
-            if (!pensioner) return Response.error(res, 404, "Pensioner does not exist");
-            await pensioner.destroy();
+      const pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
+      if (!pensioner)
+        return Response.error(res, 404, "Pensioner does not exist");
+      await pensioner.destroy();
 
-            return Response.success(res, 200, pensioner, "Pensioner has been deleted");
-        } catch (error) {
-            return Response.error(res, 404, "Pensioner does not exist");
-        }
+      return Response.success(
+        res,
+        200,
+        pensioner,
+        "Pensioner has been deleted"
+      );
+    } catch (error) {
+      return Response.error(res, 404, "Pensioner does not exist");
     }
+  }
 
-    static async getSpecificPensioner(req, res, next) {
-        try {
-            const { pensionerID } = req.params
+  static async getSpecificPensioner(req, res, next) {
+    try {
+      const { pensionerID } = req.params;
 
-            const pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
-            if (!pensioner) return Response.error(res, 404, "Pensioner does not exist");
-            return Response.success(res, 200, pensioner, "Pensioner Found");
-
-        } catch (error) {
-            return Response.error(res, 404, "Pensioner does not exist");
-        }
+      const pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
+      if (!pensioner)
+        return Response.error(res, 404, "Pensioner does not exist");
+      return Response.success(res, 200, pensioner, "Pensioner Found");
+    } catch (error) {
+      return Response.error(res, 404, "Pensioner does not exist");
     }
+  }
 
-    static async updatePensioner(req, res, next) {
-        try {
-            const { pensionerID } = req.params
-            const pensionerDetails = await validateUpdatePensioner(req.body);
-            let pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
-            if (!pensioner) return Response.error(res, 404, "Pensioner does not exist");
-            await pensioner.update(pensionerDetails, { where: { id: pensionerID } });
+  static async updatePensioner(req, res, next) {
+    try {
+      const { pensionerID } = req.params;
+      const pensionerDetails = await validateUpdatePensioner(req.body);
+      let pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
+      if (!pensioner)
+        return Response.error(res, 404, "Pensioner does not exist");
+      await pensioner.update(pensionerDetails, { where: { id: pensionerID } });
 
-            Response.success(res, 200, pensioner);
+      Response.success(res, 200, pensioner);
+    } catch (err) {
+      if (err.isJoi && err.name === "ValidationError") {
+        return res.status(400).json({
+          status: 400,
+          errors: validationResponse(err)
+        });
+      }
 
-        } catch (err) {
-            if (err.isJoi && err.name === 'ValidationError') {
-                return res.status(400).json({
-                    status: 400,
-                    errors: validationResponse(err)
-                });
-            }
-
-            if (err.errors && err.errors[0].type === 'unique violation') {
-                return res.status(400).json({
-                    status: 400,
-                    errors: validateUniqueResponse(err)
-                });
-            }
-            next(err);
-        }
+      if (err.errors && err.errors[0].type === "unique violation") {
+        return Response.error(res, 403, "Email already exists");
+      }
+      next(err);
     }
+  }
 
-    static async pay(req, res, next) {
-        try {
-            const { pensionerID } = req.params
+  static async pay(req, res, next) {
+    try {
+      const { pensionerID } = req.params;
 
-            const paymentDetails = await validatePayment(req.body);
-            const pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
-            if (!pensioner) return Response.error(res, 404, "Pensioner does not exist");
+      const paymentDetails = await validatePayment(req.body);
+      const pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
+      if (!pensioner)
+        return Response.error(res, 404, "Pensioner does not exist");
 
-            const payment = await pensioner.createPaymentHistory({
-                accountNumber: pensioner.acctNum,
-                bank: pensioner.bank,
-                ...paymentDetails,
-            });
+      const payment = await pensioner.createPaymentHistory({
+        accountNumber: pensioner.acctNum,
+        bank: pensioner.bank,
+        ...paymentDetails
+      });
 
-            return Response.success(res, 200, payment);
-
-        } catch (err) {
-            if (err.isJoi && err.name === 'ValidationError') {
-                return res.status(400).json({
-                    status: 400,
-                    errors: validationResponse(err)
-                });
-            }
-            return Response.error(res, 400, "Amount should be a number");
-        }
+      return Response.success(res, 200, payment);
+    } catch (err) {
+      if (err.isJoi && err.name === "ValidationError") {
+        return res.status(400).json({
+          status: 400,
+          errors: validationResponse(err)
+        });
+      }
+      return Response.error(res, 400, "Amount should be a number");
     }
+  }
 
-    static async getPaymentHistory(req, res, next) {
-        try {
+  static async getPaymentHistory(req, res, next) {
+    try {
+      const { pensionerID } = req.params;
 
-            const { pensionerID } = req.params
+      const pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
+      if (!pensioner)
+        return Response.error(res, 404, "Pensioner does not exist");
+      const paymentHistory = await pensioner.getPaymentHistories();
 
-            const pensioner = await Pensioner.findOne({ where: { id: pensionerID } });
-            if (!pensioner) return Response.error(res, 404, "Pensioner does not exist");
-            const paymentHistory = await pensioner.getPaymentHistories();
-
-            return Response.success(res, 200, paymentHistory);
-
-        } catch (error) {
-            return Response.error(res, 404, "Pensioner does not exist");
-        }
+      return Response.success(res, 200, paymentHistory);
+    } catch (error) {
+      return Response.error(res, 404, "Pensioner does not exist");
     }
+  }
 }
 
 export default PensionerController;
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,76 +1,84 @@
 import { validateSignup, validateLogin } from "@validations/auth";
-import { validateUniqueResponse, validationResponse } from "@helpers/validationResponse";
-import models from "@models"
-import { config } from 'dotenv';
-import jwt from "jsonwebtoken"
-import Response from "@helpers/Response"
-import bcrypt from "bcrypt"
+import {
+  validateUniqueResponse,
+  validationResponse
+} from "@helpers/validationResponse";
+import models from "@models";
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
+import Response from "@helpers/Response";
+import bcrypt from "bcrypt";
 import Token from "@middlewares/Token";
 
-const { User } = models
+const { User } = models;
 
 class UserController {
-    static async create(req, res, next) {
-        try {
-            const userDetails = await validateSignup(req.body);
-            const user = await User.create(userDetails);
+  static async create(req, res, next) {
+    try {
+      const userDetails = await validateSignup(req.body);
+      const user = await User.create(userDetails);
 
-            const payload = {
-                id: user.id,
-                email: user.email
-            };
-            const token = await Token.create(payload);
+      const payload = {
+        id: user.id,
+        email: user.email
+      };
+      const token = await Token.create(payload);
 
-            return Response.success(res, 201, { user, token }, "The user has been created successfully");
+      return Response.success(
+        res,
+        201,
+        { user, token },
+        "The user has been created successfully"
+      );
+    } catch (err) {
+      if (err.isJoi && err.name === "ValidationError") {
+        return res.status(400).json({
+          status: 400,
+          errors: validationResponse(err)
+        });
+      }
 
-        } catch (err) {
-            if (err.isJoi && err.name === 'ValidationError') {
-                return res.status(400).json({
-                    status: 400,
-                    errors: validationResponse(err)
-                });
-            }
-
-            if (err.errors && err.errors[0].type === 'unique violation') {
-                return res.status(400).json({
-                    status: 400,
-                    errors: validateUniqueResponse(err)
-                });
-            }
-            next(err);
-        }
-
+      if (err.errors && err.errors[0].type === "unique violation") {
+        return Response.error(res, 403, "Email already exists");
+      }
+      next(err);
     }
+  }
 
-    static async login(req, res, next) {
-        try {
-            const userDetails = await validateLogin(req.body);
-            const user = await User.findOne({ where: { email: userDetails.email } });
-            if (!user) { return Response.error(res, 400, "Email or Password is not correct") };
-            const match = await bcrypt.compare(userDetails.password, user.password);
-            if (!match) { return Response.error(res, 400, "Email or Password is not correct") };
+  static async login(req, res, next) {
+    try {
+      const userDetails = await validateLogin(req.body);
+      const user = await User.findOne({ where: { email: userDetails.email } });
+      if (!user) {
+        return Response.error(res, 400, "Email or Password is not correct");
+      }
+      const match = await bcrypt.compare(userDetails.password, user.password);
+      if (!match) {
+        return Response.error(res, 400, "Email or Password is not correct");
+      }
 
-            const payload = {
-                id: user.id,
-                email: user.email
-            };
-            const token = await Token.create(payload);
+      const payload = {
+        id: user.id,
+        email: user.email
+      };
+      const token = await Token.create(payload);
 
-            return Response.success(res, 200, { user, token }, "You have been logged in successfully");
-
-        } catch (err) {
-            if (err.isJoi && err.name === 'ValidationError') {
-                return res.status(400).json({
-                    status: 400,
-                    errors: validationResponse(err)
-                });
-            }
-            next(err);
-        }
+      return Response.success(
+        res,
+        200,
+        { user, token },
+        "You have been logged in successfully"
+      );
+    } catch (err) {
+      if (err.isJoi && err.name === "ValidationError") {
+        return res.status(400).json({
+          status: 400,
+          errors: validationResponse(err)
+        });
+      }
+      next(err);
     }
-
-
-
+  }
 }
 
 export default UserController;
